3dee544691546503e9524ab119abbce63bfb836d
Cyrill Podrez <CyTheRabbit@gmail.com>
2020-02-29 12:05:44 +0300

    Added application manager
    Used to initialize core game managers before any prefab uses it

---
+++ Assets/Scripts/AppManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Application entry point to initialize core game managers
+/// and to set application constants.
+/// </summary>
+public class AppManager : ScriptableObject
+{
+    [SerializeField] private int m_editorFrameRate = 60;
+    private void Init()
+    {
+#if UNITY_EDITOR
+        Application.targetFrameRate = m_editorFrameRate;
+#endif
+    }
+
+    private const string AppManagerPath = "Application Manager";
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void OnLoad() => Instantiate(Resources.Load<AppManager>(AppManagerPath)).Init();
+}
