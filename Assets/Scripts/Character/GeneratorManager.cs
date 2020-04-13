79381c3912e1b0ae53f20172184a3df9f0542d0b
Cyrill Podrez <CyTheRabbit@gmail.com>
2020-04-13 10:12:50 +0300

    Changed character generation system

---
+++ Assets/Scripts/Character/GeneratorManager.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Character
+{
+    [CreateAssetMenu(menuName = "Managers/Generator")]
+    public class GeneratorManager : BaseManager
+    {
+        [SerializeField] private GeneratorConfig m_configs = null;
+        
+        
+        public override void Init()
+        {
+        }
+
+        public string GenerateName(IEnumerable<string> tags)
+        {
+            return Utils.PickRandom(m_configs.Names);
+        }
+
+        public string GenerateDescription(string[] tags)
+        {
+            return Utils.PickRandom(m_configs.Descriptions);
+        }
+
+        public Sprite GeneratePhoto(string[] tags)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
